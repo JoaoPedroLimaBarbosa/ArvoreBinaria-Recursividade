8c79a685d4b7810291c978c290267146218aa9ec
JoaoPedroLimaBarbosa <joaopedrolimabarbosa007@gmail.com>
2025-11-09 18:57:39 -0300

    Create Node class for binary search tree
    Added a Node class to represent a node in a binary search tree.

--- Node.java
+++ Node.java
@@ -1,1 +1,14 @@
+// Node.java
+// Representa um nó da árvore binária de busca (BST).
 
+public class Node {
+    int valor;
+    Node esquerda;
+    Node direita;
+
+    public Node(int valor) {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
